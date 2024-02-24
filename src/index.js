a3cb714aa054bc69e80efe7ff9e32ddb567f0a0b
Mello0324 <mamellokhokoane@gmail.com>
2024-02-24 12:24:43 +0200

    Injected more weather data

--- src/index.js
+++ src/index.js
@@ -1,12 +1,50 @@
 function refreshWeather(response) {
 let temperatureElement= document.querySelector("#temperature");
 let temperature = response.data.temperature.current;
- let cityElement = document.querySelector("#city");
+let cityElement = document.querySelector("#city");
+let descriptionElement = document.querySelector("#description");
+let humidityElement = document.querySelector("#humidity");
+let windSpeedElement = document.querySelector("#wind-speed");
+let timeElement = document.querySelector("#time");
+let date = new Date(response.data.time * 1000);
+
+console.log(response.data.condition.description);
+
 
 cityElement.innerHTML = response.data.city;
+timeElement.innerHTML = formatDate(date);
+descriptionElement.innerHTML = response.data.condition.description;
+humidityElement.innerHTML = `${response.data.temperature.humidity}`;
 temperatureElement.innerHTML = Math.round(temperature);
+windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
 
 }
+
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thurday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = days[date.getDay()];
+
+  if (minutes <10) {
+     minutes = `0${minutes}`;
+  }
+  return `${day} ${hours}:${minutes}`;
+
+}
+
+
+
 
 function searchCity(city){
 let apiKey = "8db2acdt108befcaed4d3o9902ea6bf1";
